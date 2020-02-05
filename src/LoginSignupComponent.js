e53904a6950c1bd398e411ac2068eaacabce4ce4
SlickJacket <novenarii@gmail.com>
2020-02-05 13:07:02 -0500

    basic HTML layout complete for loginSignupComponent

--- src/LoginSignupComponent.js
+++ src/LoginSignupComponent.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class loginSignupComponent extends Component {
-    state={}
-    render() { 
-        return ( <div className="loginSignupWrapper">
-            
-        </div> );
-    }
+  state = {};
+  render() {
+    return (
+      <div className="loginSignupWrapper">
+        <div className="loginForm"></div>
+        <div className="signupForm"></div>
+      </div>
+    );
+  }
 }
- 
+
 export default loginSignupComponent;
