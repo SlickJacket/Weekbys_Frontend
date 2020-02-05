f87648c2c2ab62acaacbd7a33bc0fba2f28c7215
SlickJacket <novenarii@gmail.com>
2020-02-05 12:47:50 -0500

    landing page HTML layout complete

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,11 +1,19 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 
 class LandingPage extends Component {
     state={}
     render() { 
-        return ( <Fragment>
-            
-        </Fragment> );
+        return ( <div className="landingPageWrapper">
+            <div className="navBar">
+                <div className="logo"></div>
+                <div className="loginSignupButtons"></div>
+            </div>
+            <div className="landingContent">
+                <div className="welcome"></div>
+                <div className="missionTitle"></div>
+                <div className="missionStatement"></div>
+            </div>
+        </div> );
     }
 }
  
