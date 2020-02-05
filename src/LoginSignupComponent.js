f0d8c0cca4819c3fc4896de060e74d4550fde43e
SlickJacket <novenarii@gmail.com>
2020-02-05 13:04:28 -0500

    created loginSignupComponent

---
+++ src/LoginSignupComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class loginSignupComponent extends Component {
+    state={}
+    render() { 
+        return ( <div className="loginSignupWrapper">
+            
+        </div> );
+    }
+}
+ 
+export default loginSignupComponent;
