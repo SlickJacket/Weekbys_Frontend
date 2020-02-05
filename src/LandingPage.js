6f703ff79ae25fe10a3f67c730c49db46c3283b4
SlickJacket <novenarii@gmail.com>
2020-02-05 12:59:50 -0500

    UserPage HTML layout complete

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class LandingPage extends Component {
-    state={}
-    render() { 
-        return ( <div className="landingPageWrapper">
-            <div className="navBar">
-                <div className="logo"></div>
-                <div className="loginSignupButtons"></div>
-            </div>
-            <div className="landingContent">
-                <div className="welcome"></div>
-                <div className="missionTitle"></div>
-                <div className="missionStatement"></div>
-            </div>
-        </div> );
-    }
+  state = {};
+  render() {
+    return (
+      <div className="landingPageWrapper">
+        <div className="navBar">
+          <div className="logo"></div>
+          <div className="loginSignupButtons"></div>
+        </div>
+        <div className="landingContent">
+          <div className="welcome"></div>
+          <div className="missionTitle"></div>
+          <div className="missionStatement"></div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default LandingPage;
--- src/UserPage.js
+++ src/UserPage.js
@@ -1,12 +1,44 @@
-import React, { Component, Fragment  } from 'react';
+import React, { Component } from "react";
 
 class UserPage extends Component {
-    state={}
-    render() { 
-        return ( <Fragment>
-            
-        </Fragment> );
-    }
+  state = {};
+  render() {
+    return (
+      <div className="userPageWrapper">
+        <div className="navBar">
+          <div className="logo"></div>
+          <div className="menu"></div>
+        </div>
+        <div className="userContentContainer">
+          <div className="profileInfoContainer">
+            <div className="pic"></div>
+            <div className="name"></div>
+            <div className="completedTasks"></div>
+            <div className="todaysTasksContainer">
+              <div className="todaysTitle"></div>
+              <div className="todaysTasks"></div>
+            </div>
+          </div>
+          <div className="taskManagerContainer">
+            <div className="addTaskContainer">
+              <div className="dayTitle"></div>
+              <div className="addTaskInput"></div>
+              <div className="tasks"></div>
+            </div>
+            <div className="daysContainer">
+              <div className="days Monday"></div>
+              <div className="days Tuesday"></div>
+              <div className="days Wednesday"></div>
+              <div className="days Thursday"></div>
+              <div className="days Friday"></div>
+              <div className="days Saturday"></div>
+              <div className="days Sunday"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default UserPage;
