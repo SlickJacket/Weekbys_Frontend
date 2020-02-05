8e5213a49da008f9ffff77933a60dc7d4518dc64
SlickJacket <novenarii@gmail.com>
2020-02-05 12:41:20 -0500

    basic set up of components

---
+++ src/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react';
+
+class LandingPage extends Component {
+    state={}
+    render() { 
+        return ( <Fragment>
+            
+        </Fragment> );
+    }
+}
+ 
+export default LandingPage;
---
+++ src/UserPage.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment  } from 'react';
+
+class UserPage extends Component {
+    state={}
+    render() { 
+        return ( <Fragment>
+            
+        </Fragment> );
+    }
+}
+ 
+export default UserPage;
